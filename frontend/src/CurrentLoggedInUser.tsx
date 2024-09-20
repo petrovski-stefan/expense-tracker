97b83056669d806a34e1d974877178a264714f1c
Stefan Petrovski <spetrovski09@gmail.com>
2024-09-20 16:13:11 +0200

    Design current logged in user component

--- frontend/src/CurrentLoggedInUser.tsx
+++ frontend/src/CurrentLoggedInUser.tsx
@@ -1,8 +1,16 @@
 import useAuthContext from './auth-context/use-auth-context';
 
 export const CurrentLoggedInUser = () => {
-  const { authInfo } = useAuthContext(); // TODO: move to props?
+  const { authInfo } = useAuthContext();
   return (
-    <div className="flex items-center justify-center text-white">USER:{authInfo.username}</div>
+    <div className="flex items-center justify-center">
+      <p
+        className={`text-white font-bold ${
+          authInfo.token === '' ? 'hidden' : ''
+        } md:bg-indigo-700 rounded-full p-2`}
+      >
+        Hello, {authInfo.username}!
+      </p>
+    </div>
   );
 };
