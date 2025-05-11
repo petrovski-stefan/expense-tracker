cd777f25b8c1f7df847f40bd7971968a86ce0a24
Stefan Petrovski <spetrovski09@gmail.com>
2025-05-11 20:45:10 +0200

    feat: make backend url for api calls configurable from .env

--- frontend/src/services/custom-axios.ts
+++ frontend/src/services/custom-axios.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
-const axiosInstance = axios.create({ baseURL: 'http://127.0.0.1:8000/api' });
+const apiUrl = import.meta.env.VITE_BACKEND_URL as string;
+
+const axiosInstance = axios.create({ baseURL: apiUrl });
 
 export default axiosInstance;
